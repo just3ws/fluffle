9ecac962667f15400e9f8741b708f335cbe611e1
Mike Hall <mike@just3ws.com>
2023-06-02 07:37:16 -0500

    Update sync back

--- bin/syncer.zsh
+++ bin/syncer.zsh
@@ -1,31 +1,40 @@
 #!/usr/bin/env zsh
 # vim: set filetype=zsh:
 
+mkdir -p ~/podcasts/dead-rabbit-radio/episodes/
 cd ~/podcasts/dead-rabbit-radio/episodes/
 
 for node in node{01..05}; do
   cd ~/podcasts/dead-rabbit-radio/episodes/
 
-  /usr/local/bin/rsync -avz *.{log,srt,txt} deploy@${node}:~/podcasts/dead-rabbit-radio/episodes/ ;
-  /usr/local/bin/rsync -avz deploy@${node}:~/podcasts/dead-rabbit-radio/episodes/*.{srt,txt,log} . ;
+  # /usr/local/bin/rsync -avz *.{log,srt,txt,vtt} deploy@${node}:~/podcasts/dead-rabbit-radio/episodes/ ;
+  # /usr/local/bin/rsync -avz deploy@${node}:~/podcasts/dead-rabbit-radio/episodes/**/*.{log,srt,txt,vtt,.*} . ;
+  /usr/local/bin/rsync -avz \
+    --include '*/' \
+    --include='*.txt' \
+    --include='*.vtt' \
+    --include='*.log' \
+    --include='*.srt' \
+    --include='.*' \
+    --exclude='*' deploy@${node}:~/podcasts/dead-rabbit-radio/episodes/ ~/podcasts/dead-rabbit-radio/episodes/
 done
 
-cp -n *.{srt,txt} ~/projects/just3ws/fluffle/transcripts
+# cp -n *.{srt,txt} ~/projects/just3ws/fluffle/transcripts
 
-cd ~/projects/just3ws/fluffle/transcripts
-
-echo
-echo '------------------------------------------'
-echo
-
-ls *.srt | wc -l
+# cd ~/projects/just3ws/fluffle/transcripts
 
 echo
 echo '------------------------------------------'
 echo
 
-git add -A
+# ls *.srt | wc -l
 
-git commit -am "Transcription update"
+echo
+echo '------------------------------------------'
+echo
 
-git push
+# git add -A
+#
+# git commit -am "Transcription update"
+#
+# git push
